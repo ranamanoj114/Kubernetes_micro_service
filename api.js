5d0c9e0c1ce7b147fc67b3bd3c8b1d17c4e01fa4
ranamanoj114 <114919655+ranamanoj114@users.noreply.github.com>
2023-06-13 11:24:51 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+const { Client } = require('pg')
+const client = new Client({
+  user: 'nagp_db',
+  host: '10.8.130.18',
+  database: 'nagp_db',
+  password: 'password',
+  port: 80,
+})
+
+
 
 app.get('/', (req, res) => {
+  client.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
   res.send('Hello from App Engine!');
 });
 
